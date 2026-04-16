6a9d4a79426ff1b5fe720431bb13999d813fd477
Tarun Podilapu <vtu25134@veltech.edu.in>
2026-04-16 23:26:52 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,6 +4,6 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Tarun_143",
+        password="DB_password",
         database="feedback_db"
     )
